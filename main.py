81abf54a221b38ff037abf912a8d7612c94d6ead
weirdrah-dev <weirdrah@gmail.com>
2024-11-24 12:55:01 -0700

    created main.py and set it so it only works if the file is run directly

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+
+def main():
+    print("Starting asteroids!")
+
+if __name__ == "__main__": # Makes sure main() only runs when the file is run directly and not if imported as module
+    main()
