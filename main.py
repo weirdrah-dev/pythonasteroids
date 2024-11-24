9a94d0227678ab0189f9d8bf77cb350b79a90e16
weirdrah-dev <weirdrah@gmail.com>
2024-11-24 14:41:10 -0700

    added player so that main draws it on screen every frame.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
 from constants import * # I need to grab all the variables from the constants module
+from player import Player
 
 def main():
     pygame.init() # I am using pygame for this project and initialize it here
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get(): # This is so that the exit button works on the window the program makes
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0,0))
+        player.draw(screen)
 
         pygame.display.flip() # This updates the window every loop
         clock.tick(60)
