dec42740ee1afc085ddf4ff5a646dab6fc24826a
weirdrah-dev <weirdrah@gmail.com>
2024-11-24 14:10:30 -0700

    game loop set to 60 frames per second

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from constants import * # I need to grab all the variables from the constants mo
 
 def main():
     pygame.init() # I am using pygame for this project and initialize it here
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get(): # This is so that the exit button works on the window the program makes
             if event.type == pygame.QUIT:
@@ -11,6 +13,8 @@ def main():
         screen.fill((0,0,0,0))
 
         pygame.display.flip() # This updates the window every loop
+        clock.tick(60)
+        dt = clock.tick() / 1000
 
 if __name__ == "__main__": # Makes sure main() only runs when the file is run directly and not if imported as module
     main()
