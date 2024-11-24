b515e7c66e762445a3ac89a059f1931e69dd130e
weirdrah-dev <weirdrah@gmail.com>
2024-11-24 13:30:18 -0700

    main now creates a closable window with a black background.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
-from constants import *
+from constants import * # I need to grab all the variables from the constants module
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init() # I am using pygame for this project and initialize it here
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    while True:
+        for event in pygame.event.get(): # This is so that the exit button works on the window the program makes
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0,0))
+
+        pygame.display.flip() # This updates the window every loop
 
 if __name__ == "__main__": # Makes sure main() only runs when the file is run directly and not if imported as module
     main()
