f754b9c5fb3e6089d20aa91f634a6bc5f0eb86f9
weirdrah-dev <weirdrah@gmail.com>
2024-11-24 17:44:39 -0700

    grouped objects into updatable and drawable groups in main. assigned player to both.

--- main.py
+++ main.py
@@ -7,14 +7,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get(): # This is so that the exit button works on the window the program makes
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
 
         pygame.display.flip() # This updates the window every loop
         clock.tick(60)
